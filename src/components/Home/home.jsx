c247908a2669f90810ea6758a1f03a5ba37ef9e5
Sergiy Kushnir <s1983kushnir@gmail.com>
2023-05-30 14:52:06 +0300

    Clear from coments

--- src/components/Home/home.jsx
+++ src/components/Home/home.jsx
@@ -1,27 +1,15 @@
 import ListMovies from 'components/ListMovies/listMovies';
 import { useState, useEffect } from 'react';
-// import { Link, useLocation } from 'react-router-dom';
 import {trendMovies} from "utils/searchDataMovie";
 
 const Home = () => {
     const [movies, setMovies] = useState([])
-  // const location = useLocation()
+
  
   useEffect(() => {
         trendMovies().then(data => setMovies(data.results));
     }, []);
   return <>{movies && <ListMovies movies={movies} />}</>;
-  // <ul>
-  //   {movies.map(({ title, id, name }) => {
-  //     return (
-  //       <li key={id}>
-  //         <Link to={`movies/${id}`} state={{ from: location }}>
-  //           {title ? title : name}
-  //         </Link>{' '}
-  //       </li>
-  //     );
-  //   })}
-  // </ul>
 }
 
 export default Home;
